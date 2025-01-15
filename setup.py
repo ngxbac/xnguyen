ccb0ea7544b2f731911bdb44d4a6e12ad289712e
Xuan Bac Nguyen <xnguyen@uark.edu>
2025-01-15 12:51:50 -0600

    Update setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         name="xnguyen",
         version="0.0.1",
         author="Xuan-Bac Nguyen",
-        scripts=["xnguyen/bin/multi_submit.sh"],
+        scripts=["xnguyen/bin/multi_submit.sh", "xnguyen/bin/srun_buffer.sh"],
         author_email="ngxbac.dt@gmail.com",
         description=("Training base code"),
         packages=["xnguyen"],
